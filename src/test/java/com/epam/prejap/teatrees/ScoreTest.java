bb81335194a483cd5cb3b77c77e193ab673c83aa
Kanybek Mukalaev <m.kanybek@mail.ru>
2022-02-07 12:56:55 +0100

    add testScorePrint

--- src/test/java/com/epam/prejap/teatrees/ScoreTest.java
+++ src/test/java/com/epam/prejap/teatrees/ScoreTest.java
@@ -1,21 +1,41 @@
 package com.epam.prejap.teatrees;
 
-import org.testng.Assert;
+import com.epam.prejap.teatrees.game.Printer;
+import org.testng.*;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 @Test
 public class ScoreTest {
-    Score score = new Score(0);
-    @Test
+    Score score;
+    Printer printer;
+    @BeforeMethod
+    public void setUp() {
+        score = new Score(0);
+        printer = new Printer(System.out);
+    }
+
     public void testIncreaseScore() {
         // given
         int  move = 10;
         //when
-        for (int i = 0; i < 10; i++) {
+        for (int i = 0; i < move; i++) {
             score.increaseScore();
         }
         int point = score.points();
         //then
         Assert.assertEquals(move,point);
+    }
+    public void testPrintScore(){
+        // given
+        String  move = "56";
+
+        //when
+        for (int i = 0; i < 56; i++) {
+            score.increaseScore();
+        }
+        String point = score.toString();
+        //then
+        Assert.assertEquals(move, point);
     }
 }
