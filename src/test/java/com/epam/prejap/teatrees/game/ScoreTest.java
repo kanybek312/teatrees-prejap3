16888225559de6dbe3c9d6b147d6df3a13f97d8b
Kanybek Mukalaev <m.kanybek@mail.ru>
2022-02-09 12:54:37 +0100

    bumped up the pom

--- src/test/java/com/epam/prejap/teatrees/game/ScoreTest.java
+++ src/test/java/com/epam/prejap/teatrees/game/ScoreTest.java
@@ -2,17 +2,32 @@ package com.epam.prejap.teatrees;
 
 import com.epam.prejap.teatrees.game.Printer;
 import org.testng.*;
-import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.Test;
+import org.testng.annotations.*;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+import static org.testng.Assert.*;
+
+
+/**
+ * Print string to this class
+ * tpo check if there is anything or score appears int
+ * collect output of application single string
+ *
+ * **/
 
 @Test
 public class ScoreTest {
     private Score score;
+    private Printer printer;
+    private ByteArrayOutputStream actual;
 
     @BeforeMethod
 
     public void setUp() {
         score = new Score(0);
+        printer = printer = new Printer(new PrintStream(actual, true));
     }
 
     public void testIncreaseScore() {
@@ -24,12 +39,13 @@ public class ScoreTest {
         }
         int point = score.points();
         //then
-        Assert.assertTrue(move == point);
+        assertTrue(move == point);
     }
     public void testPrintScore(){
         // given
+        System.setOut(System.out);
         String  move = "56";
-
+        System.out.println(printer);
         //when
         for (int i = 0; i < 56; i++) {
             score.increaseScore();
